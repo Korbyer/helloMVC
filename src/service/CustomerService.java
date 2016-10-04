6e6613b906aefc2a6d70a368f92b624fd585bc9f
stelladream <neonykim@gmail.com>
2016-10-05 00:48:22 +0900

    박효열 제출하겠습니다

---
+++ src/service/CustomerService.java
@@ -0,0 +1,30 @@
+package service;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import model.Customer;
+
+public class CustomerService {
+	private Map<String, Customer> customers;
+	private static final CustomerService instance = new CustomerService();
+	public CustomerService() {
+		customers= new HashMap<String, Customer>();
+	}
+	public static CustomerService getInstance(){
+		return instance;
+	}
+	
+	public void addCustomer(Customer customer){
+		customers.put(customer.getId(),customer);
+	}
+	
+	public Customer login(String id,String password){
+		if(id!=null)
+			return customers.get(id.toLowerCase());
+		else
+			return null;
+	}
+
+	
+}
